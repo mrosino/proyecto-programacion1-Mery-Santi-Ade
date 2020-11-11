17ed03322646894c70398f366169781c406ce0f9
Santiago Matheu <69361600+Santiagomatheu@users.noreply.github.com>
2020-11-11 17:27:56 -0300

    cambios pelis

--- js/detallesPelis.js
+++ js/detallesPelis.js
@@ -16,17 +16,17 @@ window.addEventListener('load', function(){
       var atp =respuesta.adult
       document.querySelector(".descripcion").innerHTML= overview
       document.querySelector(".titulo").innerHTML="<h2>"+title+"</h2>"
-      document.querySelector(".poster").innerHTML="<img src='"+poster+"'/>"
-      document.querySelector(".puntu").innerHTML=`La puntuación es ${puntuacion}`
+      document.querySelector(".poster").innerHTML="<img src='"+poster+"'</img>"
+      document.querySelector(".puntu").innerHTML=`Puntuación ${puntuacion} estrellas`
       document.querySelector(".lanza").innerHTML=lanzamiento
      
    
      if (atp == false){
-      document.querySelector(".atp").innerHTML=`La pelicula posee contenido apto para público`
+      document.querySelector(".atp").innerHTML=`La pelicula posee contenido apto para todo público`
         
       }
       else{        
-        document.querySelector(".atp").innerHTML=`La pelicula posee contenido no-apto para público`
+        document.querySelector(".atp").innerHTML=`La pelicula posee contenido no-apto para todo público`
       }
 
     })
---
+++ js/peliculas.js
@@ -0,0 +1,117 @@
+window.addEventListener('load',function(){
+
+    fetch('https://api.themoviedb.org/3/movie/upcoming?api_key=ad5d026c61e51047e5dd51e17f8086dd&language=en-US&page=1')
+    .then(function(respuesta){
+      return respuesta.json()
+      
+    })
+    .then(function(infoPelis){
+        console.log(infoPelis.results)
+
+        let peliculas = ""
+        for (i = 0 ; i < 7 ; i++){
+            console.log(infoPelis.results[i])
+            let url= "https://image.tmdb.org/t/p/w500" +infoPelis.results[i].poster_path
+            let id= infoPelis.results[i].id
+            peliculas = peliculas+ '<a href="peliculas.html?id='+id+'"> <div class="imagenes1"><img src="'+url+'" alt=""></div></a> '
+        }
+        document.querySelector(".peliculas").innerHTML= peliculas
+        console.log(peliculas)
+    })
+
+    fetch('https://api.themoviedb.org/3/movie/upcoming?api_key=ad5d026c61e51047e5dd51e17f8086dd&language=en-US&page=2')
+    .then(function(respuesta){
+      return respuesta.json()
+      
+    })
+    .then(function(infoPelis1){
+        console.log(infoPelis1.results)
+
+        let peliculas = ""
+        for (i = 0 ; i < 7 ; i++){
+            console.log(infoPelis1.results[i])
+            let url= "https://image.tmdb.org/t/p/w500" +infoPelis1.results[i].poster_path
+            let id= infoPelis1.results[i].id
+            peliculas = peliculas+ '<a href="peliculas.html?id='+id+'"> <div class="imagenes1"><img src="'+url+'" alt=""></div></a> '
+        }
+        document.querySelector(".peliculas1").innerHTML= peliculas
+        console.log(peliculas)
+    })
+
+    fetch('https://api.themoviedb.org/3/movie/upcoming?api_key=ad5d026c61e51047e5dd51e17f8086dd&language=en-US&page=3')
+    .then(function(respuesta){
+      return respuesta.json()
+      
+    })
+    .then(function(infoPelis2){
+        console.log(infoPelis2.results)
+
+        let peliculas = ""
+        for (i = 0 ; i < 7 ; i++){
+            console.log(infoPelis2.results[i])
+            let url= "https://image.tmdb.org/t/p/w500" +infoPelis2.results[i].poster_path
+            let id= infoPelis2.results[i].id
+            peliculas = peliculas+ '<a href="peliculas.html?id='+id+'"> <div class="imagenes1"><img src="'+url+'" alt=""></div></a> '
+        }
+        document.querySelector(".peliculas2").innerHTML= peliculas
+        console.log(peliculas)
+    })
+
+    fetch('https://api.themoviedb.org/3/movie/upcoming?api_key=ad5d026c61e51047e5dd51e17f8086dd&language=en-US&page=4')
+    .then(function(respuesta){
+      return respuesta.json()
+      
+    })
+    .then(function(infoPelis3){
+        console.log(infoPelis3.results)
+
+        let peliculas = ""
+        for (i = 0 ; i < 7 ; i++){
+            console.log(infoPelis3.results[i])
+            let url= "https://image.tmdb.org/t/p/w500" +infoPelis3.results[i].poster_path
+            let id= infoPelis3.results[i].id
+            peliculas = peliculas+ '<a href="peliculas.html?id='+id+'"> <div class="imagenes1"><img src="'+url+'" alt=""></div></a> '
+        }
+        document.querySelector(".peliculas3").innerHTML= peliculas
+        console.log(peliculas)
+    })
+
+    fetch('https://api.themoviedb.org/3/movie/upcoming?api_key=ad5d026c61e51047e5dd51e17f8086dd&language=en-US&page=5')
+    .then(function(respuesta){
+      return respuesta.json()
+      
+    })
+    .then(function(infoPelis4){
+        console.log(infoPelis4.results)
+
+        let peliculas = ""
+        for (i = 0 ; i < 7 ; i++){
+            console.log(infoPelis4.results[i])
+            let url= "https://image.tmdb.org/t/p/w500" +infoPelis4.results[i].poster_path
+            let id= infoPelis4.results[i].id
+            peliculas = peliculas+ '<a href="peliculas.html?id='+id+'"> <div class="imagenes1"><img src="'+url+'" alt=""></div></a> '
+        }
+        document.querySelector(".peliculas4").innerHTML= peliculas
+        console.log(peliculas)
+    })
+
+    fetch('https://api.themoviedb.org/3/movie/upcoming?api_key=ad5d026c61e51047e5dd51e17f8086dd&language=en-US&page=6')
+    .then(function(respuesta){
+      return respuesta.json()
+      
+    })
+    .then(function(infoPelis5){
+        console.log(infoPelis5.results)
+
+        let peliculas = ""
+        for (i = 0 ; i < 7 ; i++){
+            console.log(infoPelis5.results[i])
+            let url= "https://image.tmdb.org/t/p/w500" +infoPelis5.results[i].poster_path
+            let id= infoPelis5.results[i].id
+            peliculas = peliculas+ '<a href="peliculas.html?id='+id+'"> <div class="imagenes1"><img src="'+url+'" alt=""></div></a> '
+        }
+        document.querySelector(".peliculas5").innerHTML= peliculas
+        console.log(peliculas)
+    })
+
+})
