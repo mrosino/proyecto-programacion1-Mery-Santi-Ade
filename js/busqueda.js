8a4647be3905410d7b15ec124802941501b7af23
addbuye <adebuye1@gmail.com>
2020-11-12 11:00:03 -0300

    search

--- js/busqueda.js
+++ js/busqueda.js
@@ -3,7 +3,11 @@ window.addEventListener('load', function(){
     let datoURL = new URLSearchParams(queryString);
     let query = datoURL.get('query'); 
    
-
+/*Cuando le das click en buscar lo que tiene que hacer es
+1. agarrar el valor del input del buscador
+2. asignarselo a una variable
+3. llamar a la api y pasarle esa variable */
+window
 
     fetch(`https://api.themoviedb.org/3/search/multi?api_key=3b4640a2c0443153138c528fe0e85a7a&language=es-ES&query=${query}&include_adult=false`)
     .then(function(infoSearch){
