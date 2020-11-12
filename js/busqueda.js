21190bc930e779b42e86ff4bd36b1f6aaf27d3a4
addbuye <adebuye1@gmail.com>
2020-11-12 17:13:04 -0300

    busqueda y detalles

--- js/busqueda.js
+++ js/busqueda.js
@@ -1,20 +1,14 @@
 window.addEventListener('load', function(){
     let queryString= location.search 
     let datoURL = new URLSearchParams(queryString);
-    let query = datoURL.get('query'); 
-let valorInput = document.querySelector('.buscador')
-var 
-/*Cuando le das click en buscar lo que tiene que hacer es
-1. agarrar el valor del input del buscador
-2. asignarselo a una variable
-3. llamar a la api y pasarle esa variable */
-window
+    let query = datoURL.get('busqueda'); 
+    
 
     fetch(`https://api.themoviedb.org/3/search/multi?api_key=3b4640a2c0443153138c528fe0e85a7a&language=es-ES&query=${query}&include_adult=false`)
     .then(function(infoSearch){
         return infoSearch.json()
       })
     .then(function(respuestaSearch){
-        console.log(respuestaSearch);
+        
     })
 })
