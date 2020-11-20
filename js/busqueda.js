360ee5c601f0b06c40044f0005acf3957ced4404
addbuye <adebuye1@gmail.com>
2020-11-20 14:34:24 -0300

    search

--- js/busqueda.js
+++ js/busqueda.js
@@ -3,6 +3,7 @@ window.addEventListener('load', function(){
     let datoURL = new URLSearchParams(queryString);
     let query = datoURL.get('busqueda'); 
     
+    
 
     fetch(`https://api.themoviedb.org/3/search/multi?api_key=3b4640a2c0443153138c528fe0e85a7a&language=es-ES&query=${query}&include_adult=false`)
     .then(function(infoSearch){
@@ -11,13 +12,14 @@ window.addEventListener('load', function(){
     .then(function(respuestaSearch){
       let respuestaArray = respuestaSearch.results
       let resultados = document.querySelector('.resultadosB')
-     
+      let poster= "https://image.tmdb.org/t/p/w500" + respuestaSearch.poster_path
+
         for (let i = 0; i < respuestaArray.length; i++) {
          
-          const e = respuestaArray[i];
-          console.log(e);
+          const elementosArray = respuestaArray[i];
+          console.log(elementosArray);
          //no funciona la api con las imagenes, probe con texto y funca
-          resultados.innerHTML += `<a href="detallesPelis.html?id=${e.id}"> <div class="imagenes"><h2>${e.original_name}</h2><img src="${e.poster_path}" alt=""></div></a>`
+          resultados.innerHTML += `<a href="detallesPelis.html?id=${elementosArray.id}"> <div class="imagenes"><h2 class="resultBNombres">${elementosArray.title}</h2><img src="${poster}" alt=""></div></a>`
         }
     })
 })
