8405661e9c937ce1df7e76b32c58f0c1f34e1acb
addbuye <adebuye1@gmail.com>
2020-11-23 18:32:55 -0300

    buscador normal y link al avanzado

---
+++ js/headerTardePeroSeguro.js
@@ -0,0 +1,30 @@
+// si me da el tiempo armo el header entero desde js, pero mientras voy agregando los cambios
+
+
+//buscador normal 
+window.addEventListener('load', function(){
+    let queryString= location.search 
+    let datoURL = new URLSearchParams(queryString);
+    let query = datoURL.get('busqueda'); 
+    
+    
+
+    fetch(`https://api.themoviedb.org/3/search/multi?api_key=3b4640a2c0443153138c528fe0e85a7a&language=es-ES&query=${query}&include_adult=false`)
+    .then(function(infoSearch){
+        return infoSearch.json()
+      })
+    .then(function(respuestaSearch){
+    
+      let respuestaArray = respuestaSearch.results
+      let resultados = document.querySelector('.resultadosB')
+      let poster= "https://image.tmdb.org/t/p/w500" + respuestaSearch.poster_path
+
+        for (let i = 0; i < respuestaArray.length; i++) {
+         
+          const elementosArray = respuestaArray[i];
+          console.log(elementosArray);
+       
+          resultados.innerHTML += `<a href="detallesPelis.html?id=${elementosArray.id}"> <div class="imagenes"><img src="https://image.tmdb.org/t/p/w500${elementosArray.poster_path}" alt=""></div></a>`
+        }
+    })
+})
