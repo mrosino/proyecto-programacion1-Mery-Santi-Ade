db86067e7447a83c0342e8ff9af7462b60ec9c62
addbuye <adebuye1@gmail.com>
2020-11-25 13:08:03 -0300

    search

--- js/busqueda.js
+++ js/busqueda.js
@@ -1,37 +1,24 @@
 window.addEventListener('load', function () {
   let queryString = location.search
-  let datoURL = new URLSearchParams(queryString);
-  let query = datoURL.get('busqueda');
+  let withGenres = new URLSearchParams(location.search).get("tipoGen")
+  let orden = new URLSearchParams(location.search).get("orden")
   let listGen = document.querySelector('.despegableGen')
-  //let tipo = document.querySelector('#prueba')
-  //let tipo2 = document.querySelector('#prueba2')
-  let search = document.querySelector('.favo2')
-
-//window.addEventListener('click')
-//boton se llama favo2
-
-
-
-  // Fetch del listados de generos disponibles
-  fetch('https://api.themoviedb.org/3/genre/movie/list?api_key=3b4640a2c0443153138c528fe0e85a7a&language=es-ES')
-    .then(function (respuesta) {
-      return respuesta.json()
+  
+    fetch('https://api.themoviedb.org/3/genre/movie/list?api_key=3b4640a2c0443153138c528fe0e85a7a&language=es-ES')
+    .then(function(respuesta){
+        return respuesta.json()
 
     })
+
     .then(function (listadoDeGeneros) {
 
       listadoDeGeneros.genres.forEach(genero => {
+        
         listGen.innerHTML += `<option class="valores" value="${genero.id}">${genero.name}</option>`
         
       })
     })
-    //para que funcione al apretar buscar
-    search.addEventListener('click', function(e){
-      e.preventDefault()
-  // Logica para la busqueda
-  
-  var withGenres = new URLSearchParams(location.search).get("tipoGen")
-  var orden = new URLSearchParams(location.search).get("orden")
+ 
 
   fetch(`https://api.themoviedb.org/3/discover/movie?api_key=35c3a4bec2a3c008c9fa7737b86aadc1&with_genres=${withGenres}&sort_by=${orden}`)
     .then(function (response) {
@@ -39,18 +26,27 @@ window.addEventListener('load', function () {
       
     })
     .then(function (data) {
-      var contenedorPelis = document.querySelector(".resultado1")
-      /*tipo.classList.remove('rotulosC')
-      tipo.classList.add('rotulosB')
-      tipo.innerHTML=`Peliculas`*/
+      
+      console.log(data);
 
       for (var i = 0; i < 14; i++) {
-        
-          contenedorPelis.innerHTML += `<a href="detallesPelis.html?id=${data.results[i].id}">
+        var contenedorPelis = document.querySelector(".resultado1")
+        if(data.results[i].backdrop_path==null){
+          let id= data.results[i].id
+
+          contenedorPelis.innerHTML +=`<a href="detallesPelis.html?id=${data.results[i].id}">
+            <img class="imagenesA" src='assets/img/3.png'>
+          </a>`
+          
+        }
+        else{        
+          contenedorPelis.innerHTML +=`<a href="detallesPelis.html?id=${data.results[i].id}">
             <img class="imagenesA" src='https://image.tmdb.org/t/p/original/${data.results[i].poster_path}'>
           </a>`
-      }
+        }
+    }
     })
+  
 })
 var withGenres = new URLSearchParams(location.search).get("tipoGen")
 var orden = new URLSearchParams(location.search).get("orden")
@@ -62,17 +58,21 @@ fetch(`https://api.themoviedb.org/3/discover/tv?api_key=3b4640a2c0443153138c528f
 })
 .then(function (data) {
   
-  var contenedorSeries = document.querySelector(".resultado2")
-/*
-  tipo2.classList.remove('rotulosD')
-  tipo2.classList.add('rotulosB')
-  tipo2.innerHTML=`Series`*/
-
   for (var i = 0; i < 14; i++) {
-    console.log(data)
-    contenedorSeries.innerHTML +=`<a href="detallesSeries.html?id=${data.results[i].id}">
+    var contenedorSeries = document.querySelector(".resultado2")
+    if(data.results[i].backdrop_path==null){
+      let id= data.results[i].id
+  
+      contenedorSeries.innerHTML +=`<a href="detallesPelis.html?id=${data.results[i].id}">
+        <img class="imagenesA" src='assets/img/3.png'>
+      </a>`
+      
+    }
+    else{        
+      contenedorSeries.innerHTML +=`<a href="detallesPelis.html?id=${data.results[i].id}">
         <img class="imagenesA" src='https://image.tmdb.org/t/p/original/${data.results[i].poster_path}'>
       </a>`
+    }
   }
 })
-})
+
