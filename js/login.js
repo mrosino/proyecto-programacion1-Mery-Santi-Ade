e996136e595c80409ccc688116af3215d9651b1c
Santiago Matheu <69361600+Santiagomatheu@users.noreply.github.com>
2020-11-26 19:31:28 -0300

    iniciar sesion listoooooooooo

--- js/login.js
+++ js/login.js
@@ -1,30 +1,35 @@
-window.addEventListener('load', function(){
-  let datos = location.search
-  let datosObjetos = new URLSearchParams(datos)
-  console.log(datos);
-  let usNombre = datosObjetos.get('nombre')
-  if (usNombre){
-    localStorage.setItem('nombre',usNombre)
-    let usuario = localStorage.getItem('nombre')
-    console.log(usuario);
-    let nombreUsuario = document.querySelector('.nombreUs')
-    nombreUsuario.innerHTML = usuario
-    let log = document.getElementById('log')
-    if (usuario != undefined){
-      log.style.display = 'none'
+  window.addEventListener('load', function(){
+    let datos = location.search
+    let datosObjetos = new URLSearchParams(datos)
+    console.log(datos);
+    let nombre = datosObjetos.get('uNombre')
+    let usuario = localStorage.getItem('uNombre')
+    let logU = document.getElementById('logU')
+    logU.style.display = 'none'
+    if (nombre && !usuario){
+      localStorage.setItem('uNombre',nombre)
+      console.log(usuario);
+      let nombreUsuario = document.querySelector('.nombreUs')
+      nombreUsuario.innerHTML = usuario
+      let log = document.getElementById('log')
+        log.style.display = 'none' 
+        let logU = document.getElementById('logU')
+        logU.style.display = 'block' 
+        location.reload()
+    }else if(usuario) {
+      let nombreUsuario = document.querySelector('.nombreUs')
+      nombreUsuario.innerHTML = usuario
+      let log = document.getElementById('log')
+        log.style.display = 'none'
+        let logU = document.getElementById('logU')
+        logU.style.display = 'block'
     }
-    else{
-      log.style.display = 'block'
-    }
-    
-  }
-  let logU = document.getElementById('logU')
-    if (usuario != undefined){
-      logU.style.display = 'none'
-    }
-    else{
-      logU.style.display = 'block'
-    }
+    logU.addEventListener('click',function(){
+      if (usuario){
+        localStorage.removeItem('uNombre')
+        location.href = 'index.html'
+      }
+    })
   
   
 
